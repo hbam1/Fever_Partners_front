5496c07597a91cb2b53b2009a9115dbb9aabef28
minju <iaki111@naver.com>
2024-03-02 19:46:45 +0900

    [fix] main css 수정

--- src/routes/Main.jsx
+++ src/routes/Main.jsx
@@ -29,29 +29,25 @@ function Main() {
                             <img src={logo} alt="" />
                         </span>
                         <span className={styles.header_icons}>
-                            <Link to={``} onClick={logout}>
-                                <i class="fa-solid fa-right-from-bracket"></i>
+                            <Link to={``}>
+                                <i className="fa-solid fa-right-from-bracket"></i>
                             </Link>
                             <Link to={``}>
-                                <i class="fa-solid fa-bell"></i>
+                                <i className="fa-solid fa-bell"></i>
                             </Link>
                             <Link to={``}>
-                                <i class="fa-solid fa-user"></i>
+                                <i className="fa-solid fa-user"></i>
                             </Link>
                         </span>
                     </div>
                     <div className={styles.main_top_part}>
-                        <div class={styles.main_top_part_content}>
+                        <div className={styles.main_top_part_content}>
                             <div>어서오세요&nbsp; <h1 className={styles.neon}>{data.nickname}</h1>님</div>
                             <div>
-                                <p>
                                     지금까지&nbsp;<h1 className={styles.neon}>{data.all_goals}</h1>개의 목표를 등록하고
-                                </p>
                             </div>
                             <div>
-                                <p>
                                     &nbsp;<h1 className={styles.neon}>{data.completed_goals}</h1>개의 목표를 달성했습니다
-                                </p>
                             </div>
                             <div className={styles.gage_bar}>
                                 <p>My Fever Gage</p>
@@ -67,7 +63,26 @@ function Main() {
                     </div>
                 </div>
             </div>
-            <Footer />
+            <div className={styles.footer}>
+                <Link to={``} className={styles.footer_link}>
+                    <div className={styles.footer_item}>
+                        <i className="fa-solid fa-user-group footer-icon"></i>
+                        <span className={styles.footer_text}>내 그룹</span>
+                    </div>
+                </Link>
+                <Link to={``} className={styles.footer_link}>
+                <div className={styles.footer_item}>
+                    <i className="fa-solid fa-bullseye footer-icon"></i>
+                    <span className={styles.footer_text}>내 목표</span>
+                </div>
+                </Link>
+                <Link to={`/achievement_reports_list`} className={styles.footer_link}>
+                <div className={styles.footer_item}>
+                    <i className="ri-file-list-3-line footer-icon"></i>
+                    <span className={styles.footer_text}>달성보고</span>
+                </div>
+                </Link>
+            </div>
         </div>
     );
 }
