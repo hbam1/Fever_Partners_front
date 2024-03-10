7a9ef0ab21eca3dc3ad8f5a8da286ed8518eb5c1
shash0423 <shash042319@naver.com>
2024-03-10 11:05:03 +0900

    [fix] alarm 기능 완성

--- src/routes/Alarm.jsx
+++ src/routes/Alarm.jsx
@@ -5,11 +5,23 @@ import { Link } from "react-router-dom";
 
 const Alarm = () => {
   const [alarms, setAlarms] = useState([]);
+  const [currentUser, setCurrentUser] = useState();
 
   useEffect(() => {
     AuthAPI.get("/api/alarms/list/")
       .then((response) => {
         setAlarms(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  // 현재 로그인 한 유저 정보를 가져옴
+  useEffect(() => {
+    AuthAPI.get("/api/users/current/detail/")
+      .then((response) => {
+        setCurrentUser(response.data);
       })
       .catch((error) => {
         console.error(error);
@@ -30,13 +42,15 @@ const Alarm = () => {
       <div className={styles.alarmContentMain}>
         <ul>
           {alarms.map((alarm) => (
-            //alarm 3개 구분하는 로직 구현해야 함
             <li key={alarm.id}>
               <p>
                 <span className={styles.mainColorText}>
                   {alarm.alarm_from.nickname}
                 </span>{" "}
-                님의 요청
+                {/* 현재 유저가 그룹장인지 멤버인지 구분해서 알림 표시 */}
+                {currentUser?.id === alarm.room?.master.id
+                  ? " 님이 그룹가입 신청"
+                  : " 님이 그룹가입 제안"}
               </p>
               <Link
                 to={`/alarm_detail/${alarm.id}/`}
